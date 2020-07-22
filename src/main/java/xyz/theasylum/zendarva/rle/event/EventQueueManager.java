7680c150cb6abcfa2ec7ac19bdfbeb32fd21b01d
zendarva <zendarva@gmail.com>
2020-07-21 22:52:57 -0500

    Concurrent Modification Exceptions are the Devil i tell you.

--- src/main/java/xyz/theasylum/zendarva/rle/event/EventQueueManager.java
+++ src/main/java/xyz/theasylum/zendarva/rle/event/EventQueueManager.java
@@ -1,14 +1,14 @@
 package xyz.theasylum.zendarva.rle.event;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import xyz.theasylum.zendarva.rle.event.event.Event;
-import xyz.theasylum.zendarva.rle.event.event.GuiEvent;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.*;
-import java.util.function.Consumer;
 
 public class EventQueueManager {
     private static final Logger LOG = LogManager.getLogger(EventQueueManager.class);
@@ -18,10 +18,17 @@ public class EventQueueManager {
     private static EventQueueManager myInstance;
     private Map<EventQueue, List<Object>> eventListeners;
     private Map<EventQueue, Queue<Event>> eventQueue;
+    private List<Pair<EventQueue, Event>> eventWaitingLine;
+    private List<Pair<EventQueue, Object>> subscribeWaitingLine;
+    private List<Pair<EventQueue, Object>> unsubscribeWaitingLine;
 
     private EventQueueManager() {
         eventListeners = new HashMap<>();
         eventQueue = new HashMap<>();
+        eventWaitingLine = new LinkedList();
+        subscribeWaitingLine= new LinkedList<>();
+        unsubscribeWaitingLine= new LinkedList<>();
+
         addQueue(guiEventQueue);
     }
 
@@ -39,13 +46,19 @@ public class EventQueueManager {
 
     public void raiseEvent(EventQueue queue, Event event) {
         if (!eventListeners.containsKey(queue)) {
-            LOG.error("Attepted to raise event on nonexistent queue: {}", queue.getClass().getSimpleName());
+            LOG.error("Attempted to raise event on nonexistent queue: {}", queue.getClass().getSimpleName());
             return;
         }
-        eventQueue.get(queue).add(event);
+        eventWaitingLine.add(new ImmutablePair<EventQueue, Event>(queue,event));
     }
 
     public void processEvents() {
+        eventWaitingLine.stream().forEach(f->eventQueue.get(f.getLeft()).add(f.getRight()));
+        subscribeWaitingLine.stream().forEach(f->eventListeners.get(f.getLeft()).add(f.getRight()));
+        unsubscribeWaitingLine.stream().forEach(f->eventListeners.get(f.getLeft()).remove(f.getRight()));
+        eventWaitingLine.clear();
+        subscribeWaitingLine.clear();
+        unsubscribeWaitingLine.clear();
         for (EventQueue queue : eventQueue.keySet()) {
             while (!eventQueue.get(queue).isEmpty()) {
                 Event event = eventQueue.get(queue).poll();
@@ -71,10 +84,20 @@ public class EventQueueManager {
 
     public void subscribeQueue(EventQueue queue, Object obj) {
         if (!eventListeners.containsKey(queue)) {
-            LOG.error("Attepted to subscribe to a nonexistent queue: {}", queue.getClass().getSimpleName());
+            LOG.error("Attempted to subscribe to a nonexistent queue: {}", queue.getClass().getSimpleName());
             return;
         }
-        eventListeners.get(queue).add(obj);
+        //eventListeners.get(queue).add(obj);
+        subscribeWaitingLine.add(new ImmutablePair<>(queue,obj));
+    }
+
+    public void unsubscribeQueue(EventQueue queue, Object obj) {
+        if (!eventListeners.containsKey(queue)) {
+            LOG.error("Attempted to unsubscribe from a nonexistent queue: {}", queue.getClass().getSimpleName());
+            return;
+        }
+        //eventListeners.get(queue).remove(obj);
+        unsubscribeWaitingLine.add(new ImmutablePair<>(queue,obj));
     }
 
 }
