58e42f7ef8d74898d05adde0f70c77c5c5072cf7
zendarva <zendarva@gmail.com>
2020-07-26 14:12:55 -0500

    Changes per Static Analysis. Added SetClean.

--- src/main/java/xyz/theasylum/zendarva/rle/component/Component.java
+++ src/main/java/xyz/theasylum/zendarva/rle/component/Component.java
@@ -17,27 +17,32 @@ public abstract class Component extends TileGrid implements EventListener {
     private final String paletteGroup;
     private List<Component> componentList = new ArrayList<>();
     protected TileGrid tileGrid;
-    protected Point location = new Point(0,0);
+    protected Point location = new Point(0, 0);
     protected Rectangle rect;
 
-    @Getter @Setter Consumer<Component> onChanged;
-    @Getter @Setter boolean visible=true;
-    @Getter @Setter boolean enabled=true;
+    @Getter
+    @Setter
+    Consumer<Component> onChanged;
+    @Getter
+    @Setter
+    boolean visible = true;
+    @Getter
+    @Setter
+    boolean enabled = true;
 
     protected boolean isDirty = true;
 
 
-
-
     public Component(Dimension dimension) {
-        this(dimension,"default");
+        this(dimension, "default");
 
     }
-    public Component(Dimension dimension, String paletteGroup){
+
+    public Component(Dimension dimension, String paletteGroup) {
         super(dimension);
 
         this.paletteGroup = paletteGroup;
-        rect = new Rectangle(0, 0, getWidth(),getHeight());
+        rect = new Rectangle(0, 0, getWidth(), getHeight());
     }
 
     public Point getLocation() {
@@ -46,32 +51,33 @@ public abstract class Component extends TileGrid implements EventListener {
 
     public void setLocation(Point location) {
         this.location = location;
-        rect = new Rectangle(0, 0, getWidth(),getHeight());
+        rect = new Rectangle(0, 0, getWidth(), getHeight());
     }
 
-    public void addComponent(Component component){
-        componentList.add(0,component);
+    public void addComponent(Component component) {
+        componentList.add(0, component);
     }
 
-    public List<Component> getComponents(){
+    public List<Component> getComponents() {
         return Collections.unmodifiableList(componentList);
     }
-    public void removeComponent(Component component){
+
+    public void removeComponent(Component component) {
         componentList.remove(component);
     }
 
 
-
-    public void update(Long time){}
+    public void update(Long time) {
+    }
 
     @Override
     public boolean mouseClicked(Point position, int button) {
         for (Component component : componentList) {
             if (!component.isEnabled())
                 continue;
-            if (component.contains(PointUtilities.makeRelative(position,component.location))){
-                if (component.mouseClicked(PointUtilities.makeRelative(position,component.location),button))
-                        return true;
+            if (component.contains(PointUtilities.makeRelative(position, component.location))) {
+                if (component.mouseClicked(PointUtilities.makeRelative(position, component.location), button))
+                    return true;
             }
         }
         return false;
@@ -82,7 +88,7 @@ public abstract class Component extends TileGrid implements EventListener {
         return false;
     }
 
-    public boolean contains(Point point){
+    public boolean contains(Point point) {
         return rect.contains(point);
     }
 
@@ -106,31 +112,35 @@ public abstract class Component extends TileGrid implements EventListener {
         return false;
     }
 
-    public void drawText(int x, int y, String text, Color foreground, Color background, int maxLength){
-        for (int i = 0; i < Math.min(maxLength,text.length()); i++) {
-            setTileCharacter(x+i,y,text.charAt(i));
-            setTileBackground(x+i,y,background);
-            setTileForeground(x+i,y,foreground);
+    public void drawText(int x, int y, String text, Color foreground, Color background, int maxLength) {
+        for (int i = 0; i < Math.min(maxLength, text.length()); i++) {
+            setTileCharacter(x + i, y, text.charAt(i));
+            setTileBackground(x + i, y, background);
+            setTileForeground(x + i, y, foreground);
         }
     }
 
-    public void drawText(int x, int y, String text, Color foreground, Color background){
-        drawText(x,y,text,foreground,background,text.length());
+    public void drawText(int x, int y, String text, Color foreground, Color background) {
+        drawText(x, y, text, foreground, background, text.length());
     }
 
-    public void drawText(int x, int y, String text, Color foreground, Color background, int highlight, Color highlightColor){
+    public void drawText(int x, int y, String text, Color foreground, Color background, int highlight, Color highlightColor) {
         for (int i = 0; i < text.length(); i++) {
-            setTileCharacter(x+i,y,text.charAt(i));
+            setTileCharacter(x + i, y, text.charAt(i));
             if (i == highlight)
-                setTileBackground(x+i,y,highlightColor);
+                setTileBackground(x + i, y, highlightColor);
             else
-                setTileBackground(x+i,y,background);
-            setTileForeground(x+i,y,foreground);
+                setTileBackground(x + i, y, background);
+            setTileForeground(x + i, y, foreground);
         }
     }
 
-    protected void setDirty(){
-        isDirty=true;
+    protected void setDirty() {
+        isDirty = true;
+    }
+
+    protected void setClean() {
+        isDirty = false;
     }
 
     public abstract Palette getPalette();
